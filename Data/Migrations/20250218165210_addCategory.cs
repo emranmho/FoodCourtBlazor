9bc632e39dc36d26026bb40d7f524066bb9ef170
Mohammodullah Emran <63514104+emranmho@users.noreply.github.com>
2025-02-19 00:50:50 +0600

    first migration

---
+++ Data/Migrations/20250218165210_addCategory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FoodCourtBlazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("97a92023-9cb4-43dd-bf61-897e8ed4a120"), "Fruit" },
+                    { new Guid("a9f0844c-6777-43cb-bb69-38b896524b4e"), "Vegetable" },
+                    { new Guid("dcd43899-89d8-43e1-889f-75340ab83621"), "Meat" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}

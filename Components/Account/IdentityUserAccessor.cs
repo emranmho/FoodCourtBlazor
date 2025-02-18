8a97d17d9135a4f6338c6f9db70392bfc55eca0f
Mohammodullah Emran <63514104+emranmho@users.noreply.github.com>
2025-02-18 19:32:33 +0600

    Add project files.

---
+++ Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using FoodCourtBlazor.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace FoodCourtBlazor.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
